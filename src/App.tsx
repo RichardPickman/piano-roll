ad1775b504949050b897de80279523d8f3e21830
RichardPickman <drezzerock@gmail.com>
2023-10-31 22:14:45 +0200

    move layout logic to own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,6 @@
-import { AnimatePresence, m, useSpring } from 'framer-motion';
-import { useCallback, useEffect, useRef, useState } from 'react';
-import { Card } from './components/Card';
-
-type Vegetable = {
-    id: number;
-    text: string;
-};
-
-const LAYOUT_GAP = 16;
+import { useState } from 'react';
+import { Layout } from './Layout';
+import { Vegetable } from './types';
 
 const vegetables: Vegetable[] = [
     { id: 1, text: 'Carrot' },
@@ -30,144 +23,19 @@ const vegetables: Vegetable[] = [
     { id: 18, text: 'Garlic' },
 ];
 
-// const useContainerViewBox = (box: HTMLElement | null) => {
-//     const [dimensions, setDimensions] = useState<DOMRect | null>(null);
-
-//     const calculateViewbox = useCallback(() => {
-//         if (box) {
-//             const data = box.getBoundingClientRect();
-
-//             setDimensions(data);
-//         }
-//     }, [box]);
-
-//     useEffect(() => {
-//         window.addEventListener('resize', calculateViewbox);
-
-//         return () => {
-//             window.removeEventListener('resize', calculateViewbox);
-//         };
-//     }, [calculateViewbox]);
-
-//     return dimensions;
-// };
-
-const config = {
-    stiffness: 200,
-    damping: 15,
-    mass: 0.1,
-};
-
 function App() {
     const [currentNote, setCurrentNote] = useState<Vegetable | null>(null);
-    const [dimensions, setDimensions] = useState<DOMRect | null>(null);
-    const currentVegetables = currentNote
+    const currentNotes = currentNote
         ? vegetables.filter(item => item.id !== currentNote.id)
         : vegetables;
 
-    const container = useRef<HTMLDivElement | null>(null);
-
-    const PRIMARY_WIDTH = useSpring(0, config);
-    const SECONDARY_WIDTH = useSpring(0, config);
-    const ITEM_WIDTH = useSpring(0, config);
-
-    const calculate = useCallback(() => {
-        if (dimensions) {
-            const oneFourth = dimensions.width / 4;
-            const primaryWidth = oneFourth * 3 - LAYOUT_GAP;
-
-            PRIMARY_WIDTH.set(currentNote ? primaryWidth : 0);
-            SECONDARY_WIDTH.set(currentNote ? oneFourth : dimensions.width);
-            ITEM_WIDTH.set(
-                currentNote ? oneFourth : dimensions.width / 3 - LAYOUT_GAP,
-            );
-        }
-    }, [ITEM_WIDTH, PRIMARY_WIDTH, SECONDARY_WIDTH, currentNote, dimensions]);
-
-    useEffect(() => {
-        if (container.current) {
-            const dims = container.current.getBoundingClientRect();
-            setDimensions(dims);
-        }
-    }, [container]);
-
-    useEffect(() => {
-        window.addEventListener('resize', calculate);
-
-        return () => {
-            window.removeEventListener('resize', calculate);
-        };
-    }, [calculate]);
-
-    useEffect(() => {
-        calculate();
-    }, [currentNote, calculate]);
-
     return (
-        <main className="relative flex h-screen justify-center px-6 py-4 text-white">
-            <button
-                className="absolute left-0 top-0"
-                onClick={() => setCurrentNote(null)}
-            >
-                remove
-            </button>
-            <div
-                ref={container}
-                className="relative mx-auto flex h-full max-w-7xl"
-                style={{ gap: LAYOUT_GAP }}
-            >
-                <m.div
-                    className="empty:hidden"
-                    initial={{
-                        opacity: 0,
-                    }}
-                    animate={{
-                        opacity: 1,
-                        transition: {
-                            delay: 1,
-                        },
-                    }}
-                    style={{ width: PRIMARY_WIDTH }}
-                >
-                    <AnimatePresence>
-                        {currentNote && (
-                            <m.div
-                                exit={{
-                                    top: 0,
-                                    left: 0,
-                                }}
-                                className="flex aspect-video w-full items-center justify-center rounded border bg-slate-600"
-                            >
-                                {currentNote.text}
-                            </m.div>
-                        )}
-                    </AnimatePresence>
-                </m.div>
-                <m.div
-                    className="relative flex h-full flex-wrap justify-center overflow-auto"
-                    style={{ width: SECONDARY_WIDTH, gap: LAYOUT_GAP }}
-                >
-                    {currentVegetables.map(item => (
-                        <AnimatePresence key={item.text}>
-                            <m.div
-                                className="aspect-video"
-                                exit={{
-                                    top: dimensions?.top,
-                                    left: dimensions?.left,
-                                    right: dimensions?.right,
-                                    bottom: dimensions?.bottom,
-                                    display: 'absolute',
-                                }}
-                                onClick={() => setCurrentNote(item)}
-                                style={{ width: ITEM_WIDTH }}
-                            >
-                                <Card>{item.text}</Card>
-                            </m.div>
-                        </AnimatePresence>
-                    ))}
-                </m.div>
-            </div>
-        </main>
+        <Layout
+            currentNote={currentNote}
+            notes={currentNotes}
+            onClick={note => setCurrentNote(note)}
+            onRemove={() => setCurrentNote(null)}
+        />
     );
 }
 
--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -1,12 +1,153 @@
-import { ReactNode } from "react";
+import { AnimatePresence, m, useSpring } from 'framer-motion';
+import { MouseEvent, useCallback, useEffect, useRef, useState } from 'react';
+import { Card } from '../components/Card';
+import { Vegetable } from '../types';
 
 interface Props {
-    primary: ReactNode;
-    secondary: ReactNode;
+    currentNote: Vegetable | null;
+    notes: Vegetable[];
+    onClick: (note: Vegetable) => void;
+    onRemove: () => void;
 }
 
-export const Layout = () => {
-    return (
+const LAYOUT_GAP = 16;
+const LAYOUT_COLUMNS = 3;
 
+const GRID_COLUMNS = 4;
+
+const config = {
+    stiffness: 200,
+    damping: 15,
+    mass: 0.1,
+};
+
+const cardData = {
+    shouldAppear: false,
+    rect: {
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        width: 0,
+        height: 0,
+    },
+};
+
+export const Layout = ({ currentNote, notes, onClick, onRemove }: Props) => {
+    const [dimensions, setDimensions] = useState<DOMRect | null>(null);
+    const [card, setCard] = useState(cardData);
+    const container = useRef<HTMLDivElement | null>(null);
+
+    const PRIMARY_WIDTH = useSpring(0, config);
+    const SECONDARY_WIDTH = useSpring(0, config);
+    const NOTE_WIDTH = useSpring(0, config);
+
+    const calculate = useCallback(() => {
+        if (dimensions) {
+            const onePart = dimensions.width / GRID_COLUMNS;
+            const primaryWidth = onePart * LAYOUT_COLUMNS;
+            const noteWidth = dimensions.width / LAYOUT_COLUMNS - LAYOUT_GAP;
+
+            PRIMARY_WIDTH.set(currentNote ? primaryWidth : 0);
+            SECONDARY_WIDTH.set(currentNote ? onePart : dimensions.width);
+            NOTE_WIDTH.set(currentNote ? onePart : noteWidth);
+        }
+    }, [NOTE_WIDTH, PRIMARY_WIDTH, SECONDARY_WIDTH, currentNote, dimensions]);
+
+    const handleCardClick = (
+        event: MouseEvent<HTMLDivElement>,
+        item: Vegetable,
+    ) => {
+        onClick(item);
+
+        const rect = event.currentTarget.getBoundingClientRect();
+
+        setCard({
+            shouldAppear: true,
+            rect,
+        });
+
+        setTimeout(() => setCard(cardData), 1000);
+    };
+
+    useEffect(() => {
+        if (container.current) {
+            const dims = container.current.getBoundingClientRect();
+
+            setDimensions(dims);
+        }
+    }, [container]);
+
+    useEffect(() => {
+        window.addEventListener('resize', calculate);
+
+        return () => {
+            window.removeEventListener('resize', calculate);
+        };
+    }, [calculate]);
+
+    useEffect(() => {
+        calculate();
+    }, [container, calculate]);
+
+    return (
+        <main className="relative flex h-screen justify-center px-6 py-4 text-white">
+            {card.shouldAppear && currentNote && dimensions && (
+                <m.div
+                    className="absolute"
+                    initial={card.rect}
+                    animate={dimensions}
+                >
+                    <Card>{currentNote.text}</Card>
+                </m.div>
+            )}
+            <button className="absolute left-0 top-0" onClick={onRemove}>
+                remove
+            </button>
+            <div
+                ref={container}
+                className="relative flex h-full w-full max-w-7xl"
+                style={{ gap: LAYOUT_GAP }}
+            >
+                <m.div
+                    className="empty:hidden"
+                    style={{ width: PRIMARY_WIDTH }}
+                >
+                    <AnimatePresence>
+                        {currentNote && (
+                            <m.div
+                                initial={{ display: 'none' }}
+                                animate={{
+                                    display: 'flex',
+                                    transition: {
+                                        delay: 1,
+                                    },
+                                }}
+                                exit={{
+                                    left: 0,
+                                }}
+                                className="flex aspect-video w-full items-center justify-center rounded border bg-slate-600"
+                            >
+                                {currentNote.text}
+                            </m.div>
+                        )}
+                    </AnimatePresence>
+                </m.div>
+                <m.div
+                    className="relative flex h-full flex-wrap justify-center overflow-auto"
+                    style={{ width: SECONDARY_WIDTH, gap: LAYOUT_GAP }}
+                >
+                    {notes.map(item => (
+                        <m.div
+                            className="aspect-video"
+                            onClick={event => handleCardClick(event, item)}
+                            style={{ width: NOTE_WIDTH }}
+                        >
+                            <Card>{item.text}</Card>
+                        </m.div>
+                    ))}
+                </m.div>
+            </div>
+        </main>
     );
 };
