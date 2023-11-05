a4ff5469982226b060c4084ba7fa0e8d5675316c
RichardPickman <drezzerock@gmail.com>
2023-11-05 19:12:22 +0200

    add utils functions

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,14 @@
 import { twMerge } from 'tailwind-merge';
 import { clsx, ClassValue } from 'clsx';
+import { MouseEvent } from 'react';
 
 export const cn = (...args: ClassValue[]) => twMerge(clsx(args));
+
+export const getPointerPosition = (event: MouseEvent<HTMLDivElement>) =>
+    event.clientX - event.currentTarget.getBoundingClientRect().left;
+
+export const getRandomRGB = () => ({
+    red: Math.floor(Math.random() * 255),
+    green: Math.floor(Math.random() * 255),
+    blue: Math.floor(Math.random() * 255),
+});
