f69ddd5186554bf896861d3f8b8fbe4f73d0c2f7
RichardPickman <drezzerock@gmail.com>
2023-11-05 20:50:57 +0200

    update layout

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -1,16 +1,19 @@
 import { AnimatePresence, m } from 'framer-motion';
 import { MouseEvent, useRef, useState } from 'react';
-import { Card } from '../components/Card';
 import { DoubleRange } from '../components/DoubleRange';
 import { FloatCard } from '../components/FloatCard';
 import { NoteItem } from '../components/Note';
 import { DELAY, LAYOUT_GAP } from '../constants';
+import { useCaretValue } from '../hooks/useCaretValue';
+import { useFloatValues } from '../hooks/useFloatValues';
 import { useLayout } from '../hooks/useLayout';
 import { NoteAttributes } from '../types';
 import { Cut } from '../types/doubleRange';
-import { cn, getPointerPosition } from '../utils';
-import { Secondary } from './elements/list';
-import { Primary } from './elements/main';
+import { cn } from '../utils';
+import { Secondary } from './elements/Secondary';
+import { Primary } from './elements/Primary';
+import { Caret } from '../components/Caret';
+import { Card } from '../components/Card';
 
 interface Props {
     currentNote: NoteAttributes | null;
@@ -18,24 +21,6 @@ interface Props {
     onClick: (note: NoteAttributes) => void;
     onRemove: () => void;
 }
-
-interface Card {
-    shouldAppear: boolean;
-    rect: {
-        top: number;
-        left: number;
-        width: number;
-    };
-}
-
-const initialCard: Card = {
-    shouldAppear: false,
-    rect: {
-        top: 0,
-        left: 0,
-        width: 0,
-    },
-};
 
 const variants = {
     initial: { display: 'none' },
@@ -53,131 +38,117 @@ interface ControlsProps {
     onClear: () => void;
 }
 
-const MainControls = ({ onRemove, onClear }: ControlsProps) => {
+const MainControls = ({ onRemove, onClear }: ControlsProps) => (
+    <m.div {...variants} className="flex gap-4">
+        <div
+            className="w-fit cursor-pointer rounded border bg-slate-600 px-4 py-2"
+            onClick={onRemove}
+        >
+            Close
+        </div>
+        <div
+            className="w-fit cursor-pointer rounded border bg-slate-600 px-4 py-2"
+            onClick={onClear}
+        >
+            Clear pallette!
+        </div>
+    </m.div>
+);
+
+interface MainContainerProps {
+    currentNote: NoteAttributes;
+    onRemove: () => void;
+}
+
+const MainContainer = ({ currentNote, onRemove }: MainContainerProps) => {
+    const { caret, moveCaret } = useCaretValue();
+    const [cuts, setCuts] = useState<Cut[]>([]);
+
     return (
-        <m.div {...variants} className="flex gap-4">
-            <div
-                className="w-fit cursor-pointer rounded border bg-slate-600 px-4 py-2"
-                onClick={onRemove}
+        <AnimatePresence>
+            <m.div
+                key="notes"
+                className="relative aspect-video w-full items-center justify-center rounded border"
+                {...variants}
             >
-                Close
-            </div>
-            <div
-                className="w-fit cursor-pointer rounded border bg-slate-600 px-4 py-2"
-                onClick={onClear}
-            >
-                Clear pallette!
-            </div>
-        </m.div>
+                <NoteItem attributes={currentNote} />
+                <div
+                    onClick={moveCaret}
+                    className="absolute h-full w-full bg-transparent"
+                >
+                    <DoubleRange
+                        cuts={cuts}
+                        onCutsChange={cuts => setCuts(cuts)}
+                    />
+                    <Caret caret={caret} />
+                </div>
+            </m.div>
+            <MainControls onRemove={onRemove} onClear={() => setCuts([])} />
+        </AnimatePresence>
     );
 };
 
 export const Layout = ({ currentNote, notes, onClick, onRemove }: Props) => {
     const container = useRef<HTMLDivElement | null>(null);
-    const [card, setCard] = useState(initialCard);
+    const { isActive, rect, renderFloatCard } = useFloatValues();
+
     const { mainWidth, secondaryWidth, cardWidth, containerRect } = useLayout(
         container,
         !!currentNote,
         onRemove,
     );
 
-    const renderFloatCard = (rect: DOMRect) => {
-        setCard({
-            shouldAppear: true,
-            rect,
-        });
-
-        setTimeout(() => setCard(initialCard), 1000 * DELAY);
-    };
-
     const handleCardClick = (
         event: MouseEvent<HTMLDivElement>,
         item: NoteAttributes,
     ) => {
-        const rect = event.currentTarget.getBoundingClientRect();
-
-        renderFloatCard(rect);
+        renderFloatCard(event.currentTarget.getBoundingClientRect());
         onClick(item);
     };
 
-    const [cuts, setCuts] = useState<Cut[]>([]);
-    const [caret, setCaret] = useState<number | null>(null);
-
-    const handleCaretClick = (event: MouseEvent<HTMLDivElement>) => {
-        const pointer = getPointerPosition(event);
-
-        setCaret(pointer);
+    const floatData = {
+        shouldAppear: isActive,
+        rect,
+        containerRect,
+        attributes: currentNote,
     };
 
     return (
         <main
             className={cn(
                 'relative flex h-screen justify-center px-6 py-4 text-white',
-                card.shouldAppear && 'cursor-wait',
+                isActive && 'cursor-wait',
             )}
         >
-            <FloatCard
-                shouldAppear={card.shouldAppear}
-                rect={card.rect}
-                containerRect={containerRect}
-                attributes={currentNote}
-            />
+            <FloatCard floatData={floatData} />
             <div
                 ref={container}
                 className={cn(
                     'relative flex h-full w-full max-w-7xl',
-                    card.shouldAppear && 'pointer-events-none',
+                    isActive && 'pointer-events-none',
                 )}
                 style={{ gap: LAYOUT_GAP }}
             >
                 <Primary key={currentNote?.id} width={mainWidth}>
                     {currentNote && (
-                        <AnimatePresence>
-                            <m.div
-                                key="notes"
-                                className="relative aspect-video items-center justify-center rounded border"
-                                style={{ width: mainWidth }}
-                                {...variants}
-                            >
-                                {currentNote && (
-                                    <NoteItem attributes={currentNote} />
-                                )}
-                                <div
-                                    onClick={handleCaretClick}
-                                    className="absolute h-full w-full bg-transparent"
-                                >
-                                    <DoubleRange
-                                        cuts={cuts}
-                                        onCutsChange={cuts => setCuts(cuts)}
-                                    />
-                                    <m.div
-                                        className="absolute h-full w-[1px]"
-                                        style={{
-                                            left: caret ? caret : undefined,
-                                        }}
-                                    />
-                                </div>
-                            </m.div>
-                            <MainControls
-                                onRemove={onRemove}
-                                onClear={() => setCuts([])}
-                            />
-                        </AnimatePresence>
+                        <MainContainer
+                            currentNote={currentNote}
+                            onRemove={onRemove}
+                        />
                     )}
                 </Primary>
-                <Secondary containerWidth={secondaryWidth}>
+                <Secondary width={secondaryWidth}>
                     {notes &&
                         notes.map((item, index) => (
-                            <m.div
+                            <Card
                                 key={item.id}
-                                className="aspect-video cursor-pointer bg-slate-600"
+                                className="cursor-pointer bg-slate-600"
+                                data-item={item}
                                 onClick={event => handleCardClick(event, item)}
                                 style={{ width: cardWidth }}
                             >
-                                {notes && (
-                                    <NoteItem attributes={notes[index]} />
-                                )}
-                            </m.div>
+                                <NoteItem attributes={notes[index]} />
+                            </Card>
                         ))}
                 </Secondary>
             </div>
