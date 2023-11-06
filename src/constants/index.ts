299a76accf0b1d34a47f5bb0c5fa7b802a5ac008
RichardPickman <drezzerock@gmail.com>
2023-11-06 18:40:57 +0200

    add comments for constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,11 @@
+// Layout gap is a gap between main content and list
 export const LAYOUT_GAP = 16;
+
+// MAIN_WIDTH_COLUMNS is a width of main container that is gets 3 columns out of 4
 export const MAIN_WIDTH_COLUMNS = 3;
 
+// Container width in grids
 export const GRID_COLUMNS = 4;
 
+// Delay from floating notes animation, for delayed render of actual notes
 export const DELAY = 0.5;
